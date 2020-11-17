d044cbdb04626f1af21f58acada313f4dfc49602
angadp <angadpreet.nagpal@gmail.com>
2020-11-17 22:01:35 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{width:'30%',margin:'0 auto',marginTop:'10%'}}>We have closed operations</h1>
+      <p style={{width:'30%',margin:'0 auto',marginTop:'3%'}}>We thank our partners for supporting us through the years. However due to low customer numbers, we have decided to close doors.</p>
+    </div>
+  );
+}
+
+export default App;
